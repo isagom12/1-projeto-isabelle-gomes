6f630bcfb99a088ed31bbfb1c521985f1c28f061
isa gomes <161726701+isagom12@users.noreply.github.com>
2024-07-30 00:37:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const idade = 16;
+const primeiroNome = "Isabelle";
+const sobrenome = "Gomes";
+let altura = 1.71;
+let massa = 50;
+
+let alturaAoQuadrado = altura*altura;
+let imc = massa/alturaAoQuadrado;
+console.log(`Seu nome é: ${primeiroNome}, Seu sobrenome é: ${sobrenome}`);
+console.log(`Sua Idade: ${idade}, sua altura: ${altura} e sua massa: ${massa}`);
+console.log(`O Imc de Isabelle é: ${imc}`);
